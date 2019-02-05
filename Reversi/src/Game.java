824cf077050498d3ce62f1dc8e61cb6a7469247a
jon <43426006+jcwalp@users.noreply.github.com>
2019-02-05 08:54:23 -0500

    added files

---
+++ Reversi/src/Game.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class Game {
+    //need a 2d array to hold game peices
+    public char[][] board = new char[8][8];
+    //Dummy board to be used in finding available moves
+    public char[][] moveFind = new char[8][8];
+    //X player char
+    public char x = 'X';
+    //O player char
+    public char o = 'O';
+    //Char to indicate valid move
+    public char validMove  = '#';
+
+    //main driving method for game
+    public void playGame(){
+
+    }
+
+
+    public
+
+
+}
---
+++ Reversi/src/Main.java
@@ -0,0 +1,11 @@
+//driving class for reversi game
+//creating a new instance of game
+//In the future im going to implement a graphical version of the game
+
+public class Main {
+
+    public static void main(String[] args){
+
+    }
+
+}
